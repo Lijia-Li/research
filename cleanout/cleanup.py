e0fd8f616a6d645e8e7dcb847db7200258d8d49e
Lijia Li <lil@oxy.edu>
2020-03-25 11:35:51 -0700

    Add update NP SQL count function
    note that the update cooccurance not working yet

--- cleanout/cleanup.py
+++ cleanout/cleanup.py
@@ -54,7 +54,11 @@ def extract_from_corpus(corpus):
 		corpus.cache_semantic_list(VP_list, file, "VP")
 		corpus.cache_semantic_list(NP_list, file, "NP")
 
+		print(NP_list)
 		# save to SQL counts file (TODO)
+		corpus.update_NP_count(NP_list)
+
+		# corpus.update_VP_count(VP_list)
 
 	return 
 
@@ -116,43 +120,9 @@ if __name__ == '__main__':
 
 
 
-
-# Testing Pipeline 
-# 	0. check all the files in place
-#	1. Extract manipulable noun form corpus
-# 	2. ...
-
-
-# File Session
-# 	1. 	Extracted words file
-# 		1.1 for each sentence in a file, extract (verb, prep, subj, obj)
-# 		1.2 check criteria
-# 		1.3 then store the output in a line 
-# 		e.g. Sentence: The knife cut through the rancid meat like hot butter.
-# 			 vpfile stores: "'VERB': cut, 'PREP': through, 'OBJ': meat, 'SUB': knife\n"
-#			 npfile stores: "'NOUN': meat, 'ADJ': rancid\n" & "'NOUN': butter, 'ADJ': hot\n"	
-#		or better: 
-# 			 vpfile stores: "cut(VERB) through(PREP) meat(OBJ) knife(SUB)\n"
-#			 npfile stores: "meat(NOUN) rancid(ADJ)\n" & "butter(NOUN) hot(ADJ)\n"	
-# 		Q: do we still aim to check the noun is manipulable? if so, what about sub? 
-#		   are we expecting subj and obj having different adj? if so, should we store seperatly?
-# 	2. 	SQLITE count file
-#		choice 1: 
-# 			VERB 	PREP 	OBJ 	SUBJ 	COOCUR
-# 	 		cut 	through meat 	knife 	1
-#		choice 2: (relative database) 
-#			VERB 	PREP 		VERB_ID		Noun_ID		meat_obj	knife_subj	
-#			cut 	through								1			1
-#			slice										1			
-#			CON: from verb will work, but would the noun count can only be specific to one verb set. 
-# 	3. SQLITE prob file
-#		1.1 P(Adj | Noun) 
-#		1.2 P(Verb | Noun)
-#		1.3 P(Verb | Adj)
-# 		1.4 
-
-
-
-
+# TODO: 
+	# 1. semantic -> count SQL file
+	# 2. add Lemmatizer in Extraff
+	# 3. Calculate prob via SQL.
 
 
--- cleanout/corpus.py
+++ cleanout/corpus.py
@@ -19,8 +19,10 @@
 #		 |---- sql_prob
 
 import sys
-from os import mkdir, listdir
+from os import mkdir, listdir, path
 from os.path import exists as file_exists, join as join_path, dirname, realpath
+
+import sqlite3
 
 from utils import *
 
@@ -37,19 +39,44 @@ class Corpus:
 		self.NP_extract_dir = join_path(self.dump_dir, "processed_corpus/NP")  # pylint: disable=invalid-name
 		self.VP_extract_dir = join_path(self.dump_dir, "processed_corpus/VP")  # pylint: disable=invalid-name
 		
+		self.np_count_path = join_path(self.stats_dir, "np_count.db")
+
+
 		self.cashed_prob = join_path(self.stats_dir, "cashed_prob")
 		self.sql_prob = join_path(self.stats_dir, "sql_prob")
     
 
+    # Can not Insert in to the c when using 
+	@property
+	def np_conn(self): 
+		# if db already created
+		if path.exists(self.np_count_path): 
+			print("here")
+			conn = sqlite3.connect(self.np_count_path)
+			c = conn.cursor()
+			
+		# otherwise, construct the db
+		else: 
+			conn = sqlite3.connect(self.np_count_path)
+			c = conn.cursor()
+			# Create a table with the column names
+			c.execute("""CREATE TABLE NP(
+						NOUN TEXT,
+						ADJ TEXT,
+						COOCCUR INTEGER 
+						)""")
+		return conn
+
+
 	def get_files_from_corpus(self): 
 		return get_filelist_from_folder(self.corpus_dir)
+
 
 	def cache_semantic_list(self, s_list, file, type):
 		"""cache out the semantics to processed_corpus folder
 
 		Arguments:
-			s_list {list(list(semantic))} -- e.g. [[NOUN: school ADJ: high], 
-[NOUN: place ADJ: tight]]
+			s_list {list(list(semantic))} -- e.g. [[NOUN: school ADJ: high], [NOUN: place ADJ: tight]]
 			file {str} -- path name of the origonal file
 			type {str} -- "NP" or "VP"
 		
@@ -74,13 +101,33 @@ class Corpus:
 		return
 
 
-	def main():
-		return
+	def update_NP_count(self, NP_list):
+		# conn = sqlite3.connect(self.np_count_path)
+		conn = self.np_conn;
+		c = conn.cursor()
+
+		# sql = "UPDATE NP SET COOCCUR = COOCCUR + 1 WHERE NOUN = (?) AND WHERE ADJ = (?)"
+
+		# get the np cursor
+		for np in NP_list: 
+			# TODO: update COOCURR
+			# c.execute(sql, (np[0], np[1]))
+			c.execute("INSERT INTO NP VALUES (?, ?, ?)", (np[0], np[1], 1))
+			conn.commit()
+
+		c.execute("SELECT * FROM NP")
+		# print(c.fetchall())
 
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+
+
+
+
 
 
 
