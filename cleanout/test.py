3480d21a92d0ff6b43f5612a6a413470c1634971
Lijia Li <lil@oxy.edu>
2020-04-13 13:42:33 -0700

    add dependences for text

--- cleanout/test.py
+++ cleanout/test.py
@@ -1,6 +1,20 @@
+import re
+from ast import literal_eval
+
+import spacy
+
+from extraff import *
+from utils import get_rules
+
+# Loading the tokenizer from spacy 
+SPACY_MODEL = 'en_core_web_sm'
+try:
+    NLP = spacy.load(SPACY_MODEL)
+except OSError:
+    spacy.cli.download(SPACY_MODEL)
+    NLP = spacy.load(SPACY_MODEL)
 
 def benchmark():
-
     def print_failed(doc, actual, expected):
         print(f'Failed sentence "{sentence}":')
         print(f'    expected:')
@@ -18,6 +32,7 @@ def benchmark():
         print(f'    parse tree:')
         print_parse_tree(doc, prefix='        ')
 
+    rules = get_rules("./rules.txt")
     count = 0
     incorrect = 0
     with open('benchmark') as fd:
@@ -32,7 +47,7 @@ def benchmark():
             else:
                 if sentence is not None:
                     doc = NLP(sentence)
-                    actual = extract_semantics(doc)
+                    actual = extract_semantics(doc, rules)
                     if actual != extractions:
                         print_failed(doc, actual, extractions)
                         incorrect += 1
@@ -41,4 +56,5 @@ def benchmark():
                 extractions = set()
     print(f'Score: {count - incorrect} / {count}')
 
-
+if __name__ == "__main__":
+    benchmark()
